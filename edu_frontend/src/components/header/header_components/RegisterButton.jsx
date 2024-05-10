04136cc4e1a111f08e43c85106e469f61eded5e7
unknown <fredileader@gmail.com>
2024-05-10 03:41:01 +0100

    made changes in component

--- edu_frontend/src/components/header/header_components/RegisterButton.jsx
+++ edu_frontend/src/components/header/header_components/RegisterButton.jsx
@@ -4,9 +4,9 @@ import { Link } from "react-router-dom";
 const RegisterButton = () => {
   return (
     <div className="reg_btn">
-      <Link to='/register'>
+      <a href='https://keyring.network/'>
         <button>Visit Site</button>
-      </Link>
+      </a>
     </div>
   )
 }
