aa5c552b0476bda4ec46ac89c4421181fa7f5e06
unknown <fredileader@gmail.com>
2024-05-10 03:24:58 +0100

    deleted backend

--- edu_backend/config/allowedOrigins.js
+++
@@ -1,8 +0,0 @@
-const allowedOrigins = [
-    'http://127.0.0.1:5500',
-    'http://localhost:3500',
-    'http://127.0.0.1:3000',
-    'https://www.google.com/',
-];
-
-module.exports = allowedOrigins;
